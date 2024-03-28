cdf1b8185e678de5f1ff1cad2bc6f639e5519c29
itobuz <itobuz@itobuzs-MacBook-Air.local>
2024-03-28 18:51:14 +0530

    first commit

---
+++ src/component/navbar.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './navbar.css';
+
+
+
+function Navbar() {
+ 
+
+  return (
+    <>
+    <div className='navSection'>
+        <h2>FLIPCARD</h2>
+        <input/>
+        <input/>
+        <div className='login'></div>
+        <div className='card'></div>
+        <div className='becomeSeller'></div>
+        
+    </div>
+    </>
+  )
+}
+
+export default Navbar
